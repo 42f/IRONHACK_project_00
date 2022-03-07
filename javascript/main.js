692cec2e750bdfa5827a0e8a9d9d23cb2b9eefe3
bvalette <bvalette@student.42.fr>
2022-03-07 11:45:17 +0100

    setup basic html strucure + icones

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,10 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  document.write("mobile device");
-}else{
-  // false for not mobile device
-  document.write("not mobile device");
-}
+
+// detect mobile client
+
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+//   // true for mobile device
+//   document.write("mobile device");
+// }else{
+//   // false for not mobile device
+//   document.write("not mobile device");
+// }
