e6129dcc5667c131cd85716e7e910c2df746c697
bvalette <bvalette@student.42.fr>
2022-03-07 14:57:51 +0100

    add toggle elements (modals, volume)

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,62 @@
 //   // false for not mobile device
 //   document.write("not mobile device");
 // }
+
+const state = {
+	displayInfos: false,
+	volume: false,
+	splash: false
+}
+
+class Game {
+	constructor(level) {
+		this.lvl = level;
+		console.log('Instanciate game with lvl', level );
+	}
+}
+
+function setVisibility(element, visibilityProperty) {
+	element.style.visibility = visibilityProperty;
+}
+
+function toggleInfos() {
+	state.displayInfos = !state.displayInfos;
+	const infosModal = document.querySelector('#infos');
+	state.displayInfos ? setVisibility(infosModal, 'visible') : setVisibility(infosModal, 'hidden')
+}
+
+function toggleSplashScreen() {
+	state.splash = !state.splash;
+	const splashModal = document.querySelector('#splash');
+	state.splash ? setVisibility(splashModal, 'visible') : setVisibility(splashModal, 'hidden')
+}
+
+function toggleVolume() {
+	state.volume = !state.volume;
+	const mutted = document.querySelector('#muteOn');
+	const unmutted = document.querySelector('#muteOff');
+	if (state.volume) {
+		setVisibility(unmutted, 'visible')
+		setVisibility(mutted, 'hidden')
+	} else {
+		setVisibility(mutted, 'visible')
+		setVisibility(unmutted, 'hidden')
+	}
+}
+
+function startGame(event) {
+	const game = new Game(event.target.dataset.lvl);
+
+	toggleSplashScreen();
+
+}
+
+toggleVolume();
+
+// Commented for debug
+// toggleSplashScreen();
+
+document.querySelector('#info-btn')?.addEventListener('click', toggleInfos);
+document.querySelector('#volume')?.addEventListener('click', toggleVolume);
+const levelButtons = document.querySelectorAll('#splash .button');
+levelButtons.forEach(btn => btn.addEventListener('click', startGame));
