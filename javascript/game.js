093580e74d5042568f028c4379cada64fd3a73a9
bvalette <bvalette@student.42.fr>
2022-03-09 09:58:29 +0100

    refractor and cleanup game logic

--- javascript/game.js
+++ javascript/game.js
@@ -30,51 +30,50 @@ class Grid {
 ===================================================================
 */
 
-
 class Game {
 	constructor(level) {
 		document.addEventListener('click', (event) => { this.clickHandler(event) });
-		this.timerBip = new Audio('./media/audio/bip.mp3');
+		// this.timerBip = new Audio('./media/audio/bip.mp3');
 		this.timerElement = document.querySelector('#tourTimer');
 		this.scoreElement = document.querySelector('#score');
-
-		this.timer = new Countdown();
+		this.countdown = new Countdown();
 		this.guessCouple = [];
+		this.guessTimeoutId = null;
 		this.state = {
-			timeOut: false,
+			overTime: false,
 			score: 0,
 			lvl: level,
 		}
-
 		this.grid = new Grid();
-		this.cardSet = new RandomNbrCardSet(3);
+		this.cardSet = new RandomNbrCardSet(2);
 
 		// this.cardSet.logCards();
 		// this.setCardsInGridRandomly();
 		this.setCardsInGridInOrder();
 
-		setTimeout(() => {
-			for (let card of this.grid.cards) {
-				card.hideCard(false);
-			}
-		}, 1200);
+		// setTimeout(() => {
+		// 	for (let card of this.grid.cards) {
+		// 		card.hideCard(false);
+		// 	}
+		// }, 1200);
 	}
 
 	/* USER INTERACTIONS         -----------------------------------------------*/
 
-	validClic(event) {
+	isValidClick(event) {
 		return event.target.classList.contains('card')
+			&& !event.target.classList.contains('success')
 			&& this.guessCouple.length < 2
-			&& !this.state.timeOut;
+			&& !this.state.overTime;
 	}
 
 	clickHandler(event) {
-		if (this.validClic(event)) {
-			const clickedCard = this.cardSet.cards.find(elem =>
+		if (this.isValidClick(event)) {
+			const clickedCard = this.cardSet.findCard(elem =>
 				elem.getElement() === event.target
 			);
 			if (clickedCard && !clickedCard.getSneakPeakState()) {
-				clickedCard.sneakPeakCard();
+				clickedCard.setSneakPeak();
 				this.addToGuess(clickedCard);
 			}
 		}
@@ -82,28 +81,54 @@ class Game {
 
 	addToGuess(card) {
 		this.guessCouple.push(card);
-		if (this.guessCouple.length < 2) {
-			console.log('waiting');
-			setTimeout(() => {
-				this.guessCouple = [];
-			}, 1000);
-			return 'waiting';
-		} else {
-			const sameCards = RandomNbrCardSet.cardCmp(this.guessCouple[0], this.guessCouple[1]);
-			console.log(sameCards ? 'correct' : 'incorrect');
-			if (sameCards) {
-				this.guessCouple.forEach(card => card.setCardAsFound())
-				this.incrementScore();
-			}
-			this.guessCouple = [];
-			return sameCards ? 'correct' : 'incorrect';
-		}
+		this.guessCouple.length < 2 ? this.waitForSecondCard() : this.checkGuess(card);
 	}
 
+	checkGuess() {
+		const sameCards = this.cardSet.cardCmp(this.guessCouple[0], this.guessCouple[1]);
+		sameCards ? this.manageCorrectGuess() : this.abortGuess(true);
+	}
+
+	waitForSecondCard() {
+		this.guessTimeoutId = setTimeout(() => {
+			this.abortGuess(true);
+		}, 1000);
+	}
+
+	abortGuess(isFailure) {
+		if (isFailure) {
+			this.guessCouple.forEach(card => {
+				card.unsetSneakPeak();
+				setTimeout(() => {
+					card.hideCard(true);
+				}, 500);
+			})
+		}
+		this.guessCouple = [];
+	}
+
+	manageCorrectGuess() {
+		clearTimeout(this.guessTimeoutId);
+		this.guessTimeoutId = null;
+
+		this.guessCouple.forEach(card => card.setCardAsFound())
+		this.incrementScore();
+		this.guessCouple = [];
+		this.nextTour();
+	}
 
 	incrementScore() {
 		this.state.score++;
 		this.scoreElement.innerText = this.state.score;
+	}
+
+	nextTour() {
+		this.countdown.stop();
+		this.startTimer();
+		console.log('next tour', this.cardSet.isAllFound());
+		if (this.cardSet.isAllFound()) {
+			this.endGame('win');
+		}
 	}
 
 	/* GRID MANIPULATION METHODS -----------------------------------------------*/
@@ -111,11 +136,13 @@ class Game {
 	setCardsInGridRandomly() {
 		const tmpCards = [...this.cardSet.cards];
 		while (tmpCards.length) {
-			const randomCard = tmpCards.splice(Math.floor(Math.random() * tmpCards.length), 1);
+			const randomIndex = Math.floor(Math.random() * tmpCards.length);
+			const randomCard = tmpCards.splice(randomIndex, 1);
 			this.grid.addCard(randomCard[0]);
 		}
 	}
 
+	// for debug purposes
 	setCardsInGridInOrder() {
 		for (let i = 0; i < this.cardSet.cards.length; i++) {
 			this.grid.addCard(this.cardSet.cards[i]);
@@ -125,37 +152,44 @@ class Game {
 	/* TIMER MANIPULATIONS       -----------------------------------------------*/
 
 	startTimer() {
-		this.timer.start(999, () => {
-			this.renderTime(this.timer.getRemainingSeconds(), this.timer.split());
-			if (!this.state.timeOut && this.timer.getRemainingSeconds() <= 0) {
-				this.state.timeOut = true;
-			}
+		this.countdown.start(16, () => {
+			this.timerCallback()
 		});
+	}
+
+	timerCallback() {
+		this.renderTime(this.countdown.getRemainingSeconds(), this.countdown.split());
+
+		if (!this.state.overTime && this.countdown.getRemainingSeconds() <= 0) {
+			this.state.overTime = true;
+			this.endGame();
+		}
 	}
 
 	startGame() {
 		this.startTimer();
 	}
 
-	stopGame() {
-		this.timer.stop();
-	}
-
-	nextTour() {
-		this.timer.reset();
-		this.startTimer();
+	endGame(hasWin) {
+		this.countdown.stop();
+		if (hasWin && hasWin === 'win') {
+			this.timerElement.classList.add('countdown-win');
+			console.log('WIN!');
+		} else {
+			console.log('GAME OVER');
+		}
 	}
 
 	renderTime(remainingSeconds, timeStr) {
 		if (remainingSeconds >= 0) {
 			this.timerElement.innerText = timeStr;
 			if (remainingSeconds < 25) {
-				this.timerElement.classList.toggle('timer-reach-limit');
-				// play bip warning the end of timer
+				this.timerElement.classList.toggle('countdown-reach-limit');
+				// play bip warning the end ofcountdown
 				// this.timerBip.play();
 			}
 		} else {
-			this.timerElement.classList.add('timer-reach-limit');
+			this.timerElement.classList.add('countdown-reach-limit');
 		}
 	}
 }
